56fe62c54387ab69af5a5e34ae611885158442ed
Bui Tien Thanh <tienthanh@buis-macbook-pro.local>
2018-06-30 15:43:22 +0700

    Initial Commit

---
+++ HelloWorld/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  HelloWorld
+//
+//  Created by Bui Tien Thanh on 6/30/18.
+//  Copyright © 2018 Bui Tien Thanh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
